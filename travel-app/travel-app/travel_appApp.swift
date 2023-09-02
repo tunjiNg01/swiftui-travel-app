87a365c3e54f1d0a0e51d50337b05df6d8afebab
tunjiNg01 <nahtlosng@gmail.com>
2023-09-02 13:37:46 +0100

    init code repo

---
+++ travel-app/travel-app/travel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_appApp.swift
+//  travel-app
+//
+//  Created by MACBOOK PRO on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct travel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
