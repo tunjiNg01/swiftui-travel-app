81d81168c0e9cf02a5b0255b4b856416c387ebbf
tunjiNg01 <nahtlosng@gmail.com>
2023-09-18 16:22:23 +0100

    refactor the flight page

--- travel-app/travel-app/Views/Flights.swift
+++ travel-app/travel-app/Views/Flights.swift
@@ -18,10 +18,7 @@ struct Flights: View {
     @State var passengerCount: String = "2"
     var body: some View {
         VStack {
-            ZStack {
-                Color.theme.background
-                    .ignoresSafeArea()
-            }
+          
             VStack(spacing: 8) {
                 VStack(spacing: 6) {
                     distanceLabel
@@ -39,108 +36,9 @@ struct Flights: View {
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .padding(.horizontal)
             searchButton
-            
-            VStack {
-                VStack( alignment: .leading, spacing: 8) {
-                    Text("London Airport")
-                        .font(.subheadline)
-                        .bold()
-                        .foregroundColor(Color.theme.textColor)
-                    VStack {
-                        HStack {
-                            VStack {
-                                HStack {
-                                    Text("From")
-                                    Spacer()
-                                    Text("to")
-                                }
-                                .font(.subheadline)
-                                .bold()
-                                .foregroundColor(.gray)
-                                HStack {
-                                    Circle()
-                                        .fill(.gray)
-                                        .frame(width: 5)
-                                    
-                                    Rectangle()
-                                       
-                                        .stroke(style: StrokeStyle(dash: [7.0, 8.0]))
-                                        .fill(.gray)
-                                    .frame(width: 100, height: 1 )
-                                    
-                                    Circle()
-                                        .fill(Color.theme.accent)
-                                        .frame(width: 5)
-                                }
-                                
-                                HStack {
-                                    Text("06: 00 M")
-                                    Spacer()
-                                    Text("12: 00 PM")
-                                }
-                                .padding(.vertical, 3.5)
-                                .font(.caption)
-                                .bold()
-                                .foregroundColor(.gray)
-                                
-                                HStack {
-                                    Text("Rp 62.00")
-                                        .bold()
-                                    .font(.caption)
-                                    Spacer()
-                                    Text("14 rem")
-                                        .font(.caption2)
-                                        .bold()
-                                        .foregroundColor(Color.theme.accent)
-                                }
-                                    
-                            }
-                            Spacer()
-                          Image("flightslogo1")
-                                .frame(width: UIScreen.main.bounds.width / 2, alignment: .trailing)
-                        }
-                        HStack {
-                            Text("Busines class")
-                                .font(.caption2)
-                                .bold()
-                                .foregroundColor(Color.theme.accent)
-                                .padding(.horizontal)
-                                .padding(.vertical, 4)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 4)
-                                        .fill(Color.theme.accent.opacity(0.1))
-                                   
-                                }
-                            Spacer()
-                            Text("2 hrs 18 minutes")
-                                .font(.caption)
-                                .bold()
-                                .foregroundColor(Color.theme.textColor)
-                            Spacer()
-                            Button {
-                                
-                            } label: {
-                                Text("Book")
-                                    .font(.caption)
-                                    .foregroundColor(.white)
-                                    .bold()
-                                    .padding(.vertical, 6)
-                                    .padding(.horizontal)
-                                    .background(Color.theme.accent)
-                                    .clipShape(RoundedRectangle(cornerRadius: 6))
-                            }
-
-                        }
-                    }
-                    
-                    //.background(.blue)
-                }
+            FlightRow()
                 .padding()
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-              
-           
-            } .padding()
+ 
         }
        
         
